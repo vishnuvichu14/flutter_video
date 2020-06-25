ca8ab6bd2083466618d96f50b6959c53253717e1
vishnuvarshan <varshan.17mc@kct.ac.in>
2020-06-26 01:36:01 +0530

    Uploading to git

---
+++ android/app/src/main/kotlin/com/vishnuvarshan/videoflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vishnuvarshan.videoflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+void main()=> runApp(VideoDemo());
+
+class VideoDemo extends StatefulWidget {
+  @override
+  VideoDemoState createState() => VideoDemoState();
+}
+
+class VideoDemoState extends State<VideoDemo> {
+
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    _controller = VideoPlayerController.network(
+        "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4");
+    //_controller = VideoPlayerController.asset("videos/sample_video.mp4");
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
+    _controller.setVolume(1.0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Video Demo"),
+        ),
+        body: FutureBuilder(
+          future: _initializeVideoPlayerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Center(
+                child: AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                ),
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              if (_controller.value.isPlaying) {
+                _controller.pause();
+              } else {
+                _controller.play();
+              }
+            });
+          },
+          child:
+          Icon(_controller.value.isPlaying ? Icons.pause : Icons.play_arrow),
+        ),
+      ),
+    );
+  }
+}
